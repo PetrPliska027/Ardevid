3063610a8db70f3b6e67febc40a6a25f4402da1c
Petr Pliska <fogzi007@seznam.cz>
2024-01-05 18:16:56 +0100

    Tilemap, Scene, Interactable
    ~ Completed Ruletile
    + Added new scene (Testing)
    + Added elevator (Absolutely not completed)

---
+++ Assets/ObjectMover.cs
@@ -0,0 +1,11 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMover : MonoBehaviour
+{
+    public List<GameObject> checkpoints = new List<GameObject>();
+    [SerializeField] private float speed = 4.5f;
+
+
+}
